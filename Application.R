f10a893948b33dc649d6d5f552fc654a5ea1881f
Anne-lauretho <anne-laure.thomas@etu.umontpellier.fr>
2025-02-28 16:20:21 +0100

    Modification

--- Application.R
+++ Application.R
@@ -1,18 +1,30 @@
 install.packages(c("shiny", "shinydashboard", "DT"))
+install.packages("shinycssloaders")
+install.packages("shinyjs")
 
 # Chargement des bibliothèques
 library(shiny)
 library(shinydashboard)
 library(DT)
+library(shinycssloaders)
+library(shinyjs)
 
-# Fonction pour générer une grille vide
-generate_grid <- function(size = 4) {
-  matrix("", nrow = size, ncol = size)
+# Fonction pour générer une grille vide en fonction du niveau avec des traits de séparation
+generate_grid <- function(level) {
+  size <- switch(level,
+                 "Facile" = 6,
+                 "Moyen" = 8,
+                 "Difficile" = 10,
+                 4)
+  grid <- matrix("", nrow = size, ncol = size)
+  colnames(grid) <- seq(1, size)
+  rownames(grid) <- seq(1, size)
+  grid
 }
 
 # UI - Interface utilisateur
 ui <- dashboardPage(
-  skin = "purple", 
+  skin = "purple",
   dashboardHeader(title = "Takuzu_M1_SSD"),
   
   dashboardSidebar(
@@ -24,6 +36,24 @@ ui <- dashboardPage(
   ),
   
   dashboardBody(
+    useShinyjs(),
+    tags$head(tags$style(HTML("
+      .dataTable td, .dataTable th {
+        text-align: center;
+        vertical-align: middle;
+        width: 40px;
+        height: 40px;
+      }
+      .dataTables_wrapper {
+        margin: 0 auto;
+      }
+      .table-bordered {
+        border: 1px solid #ddd;
+      }
+      .table-bordered td, .table-bordered th {
+        border: 1px solid #ddd !important;
+      }
+    "))),
     tabItems(
       
       # Onglet "Règles"
@@ -36,7 +66,7 @@ ui <- dashboardPage(
                 tags$li("Chaque ligne et chaque colonne doit être unique."),
                 tags$li("Remplissez la grille en respectant ces contraintes !")
               ),
-              img(src = "takuzu.png", width = "50%") 
+              img(src = "www/takuzu.png", width = "50%")
       ),
       
       # Onglet "Jouer"
@@ -50,18 +80,20 @@ ui <- dashboardPage(
                          selectInput("errors", "Nombre d'erreurs autorisées", choices = c(3, 5, 10)),
                          selectInput("hints", "Aides", choices = c(1, 3, 5)),
                          div(style = "text-align:center;",
-                             img(src = "magic_wand.png", height = "30px"), 
-                             img(src = "magic_wand.png", height = "30px"), 
-                             img(src = "magic_wand.png", height = "30px")
+                             img(src = "www/magic_wand.png", height = "30px"), 
+                             img(src = "www/magic_wand.png", height = "30px"), 
+                             img(src = "www/magic_wand.png", height = "30px")
                          ),
-                         verbatimTextOutput("timer")
+                         verbatimTextOutput("timer"),
+                         actionButton("validate", "Valider la grille") # Bouton de validation
                        )
                 ),
                 column(width = 8,
                        h4("Grille de jeu"),
-                       DTOutput("takuzu_grid") # Grille interactive
+                       withSpinner(DTOutput("takuzu_grid"), type = 6) # Grille interactive avec chargement
                 )
-              )
+              ),
+              verbatimTextOutput("validity_result") # Afficher le résultat de la validation
       ),
       
       # Onglet "Records"
@@ -73,36 +105,55 @@ ui <- dashboardPage(
   )
 )
 
-# SERVER - Logique de l'application
+# Serveur - Logique de l'application
 server <- function(input, output, session) {
+  # Grille initiale
+  grid <- reactiveVal(generate_grid("Facile"))
   
-  # Stocker la grille de jeu réactive
-  game_grid <- reactiveVal(generate_grid(4))
+  # Générer une nouvelle grille en fonction du niveau sélectionné
+  observeEvent(input$level, {
+    grid(generate_grid(input$level))
+  })
   
+  # Afficher la grille
   output$takuzu_grid <- renderDT({
-    datatable(game_grid(), editable = "cell", options = list(dom = 't'))
+    datatable(grid(), selection = "none", editable = TRUE,
+              options = list(dom = "t", ordering = FALSE, 
+                             rowCallback = JS('function(row, data, index) {
+                              $(row).children().each(function(i){
+                                $(this).css("border", "1px solid #ddd");
+                              });
+                            }')
+              ), class = 'table-bordered')
   }, server = FALSE)
   
-  # Mise à jour de la grille quand l'utilisateur modifie une case
-  observeEvent(input$takuzu_grid_cell_edit, {
-    info <- input$takuzu_grid_cell_edit
-    grid <- game_grid()
-    grid[info$row, info$col] <- as.character(info$value)
-    game_grid(grid)
+  # Vérifier la validité de la grille
+  observeEvent(input$validate, {
+    validity <- check_validity(grid())
+    output$validity_result <- renderText({
+      if (validity) {
+        "La grille est valide !"
+      } else {
+        "La grille n'est pas valide. Réessayez !"
+      }
+    })
   })
   
-  # Timer (statique pour le moment)
+  # Timer (simulé ici comme un texte statique)
   output$timer <- renderText({
-    "00:00" # On ajoutera un vrai chronomètre après
+    paste("Temps écoulé : 00:00") #A changé
   })
   
-  # Stocker les records des joueurs
-  records <- reactiveVal(data.frame(Joueur = character(), Temps = character(), stringsAsFactors = FALSE))
-  
+  # Afficher les records (simulés ici comme un tableau vide)
   output$record_table <- renderDT({
-    datatable(records(), options = list(pageLength = 5))
+    datatable(data.frame(
+      Joueur = character(),
+      Niveau = character(),
+      Temps = character()
+    ))
   })
 }
 
-# Lancer l'application Shiny
-shinyApp(ui = ui, server = server)
+# Lancer l'application
+shinyApp(ui, server)
+
